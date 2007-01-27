f32a8ff65dfd982aecae1f294cb86b9b41a3c030
ptdtv8yr <ptdtv8yr@6f7ed374-3227-0410-bbb7-b8272cb5e44e>
2007-01-27 22:08:36 +0000

    sample data for users table

---
+++ dev/sql/chuckwalla.sql
@@ -0,0 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 2.9.0.2
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Erstellungszeit: 27. Januar 2007 um 23:07
+-- Server Version: 5.0.27
+-- PHP-Version: 5.1.6
+
+SET AUTOCOMMIT=0;
+START TRANSACTION;
+
+-- 
+-- Datenbank: `chuckwalla`
+-- 
+
+-- 
+-- Daten für Tabelle `user`
+-- 
+
+INSERT INTO `user` VALUES 
+(1, 'dzuelke@gmail.com', '46f94c8de14fb36680850768ff1b7f2a', '1', '1', '2007-01-27 21:17:01', '2007-01-27 21:17:01', 'de@locale=Europe/Berlin'),
+(2, 'ross.lawley@gmail.com', '46f94c8de14fb36680850768ff1b7f2a', '1', '1', '2007-01-27 21:17:01', '2007-01-27 21:17:01', 'en@locale=Europe/London');
+
+COMMIT;
