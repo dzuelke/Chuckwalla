5881a0bae08d18510a6db03f9494d16addb97aa0
ptdtv8yr <ptdtv8yr@6f7ed374-3227-0410-bbb7-b8272cb5e44e>
2007-01-27 15:17:44 +0000

    assign content type in view setup methods

--- app/modules/Web/lib/view/ChuckwallaWebBaseView.class.php
+++ app/modules/Web/lib/view/ChuckwallaWebBaseView.class.php
@@ -6,18 +6,21 @@ class ChuckwallaWebBaseView extends ChuckwallaBaseView
 	{
 		// layout "standard" in output type "html"
 		$this->loadLayout('standard');
+		$this->setAttribute('_contentType', $this->container->getOutputType()->getParameter('Content-Type'));
 	}
 	
 	public function setupJson(AgaviRequestDataHolder $rd)
 	{
 		// layout "standard" in output type "json"
 		$this->loadLayout('standard');
+		$this->setAttribute('_contentType', $this->container->getOutputType()->getParameter('Content-Type'));
 	}
 	
 	public function setupAtom(AgaviRequestDataHolder $rd)
 	{
 		// layout "standard" in output type "atom"
 		$this->loadLayout('standard');
+		$this->setAttribute('_contentType', $this->container->getOutputType()->getParameter('Content-Type'));
 	}
 }
 
