74d806dc548d848b20e885b487a2f0116e786063
ptdtv8yr <ptdtv8yr@6f7ed374-3227-0410-bbb7-b8272cb5e44e>
2007-01-28 04:50:22 +0000

    sidebar

--- app/modules/Web/templates/ChannelListSuccess.php
+++ app/modules/Web/templates/ChannelListSuccess.php
@@ -1,4 +1,4 @@
-<?php foreach ($t['channels'] as $channel): ?>
+<!-- <?php foreach ($t['channels'] as $channel): ?>
 <dl id="<?php echo $channel->id; ?>" class="toggles">
 	<dt class="toggleHeader channel"><?php echo $channel->name; ?></dt>
 	<dd class="channelInformation"><?php echo $channel->number_of_members; ?> Member
@@ -11,15 +11,35 @@
 	</dd>
 </dl>
 <?php endforeach; ?>
-
+ -->
 <ul>
 	<li>
-		<div>#agavi</div>
+		<div>#agavi (15)</div>
 		<ul>
-			<li><a href="#">splatch</a></li>
-			<li><a href="#">MrJeep</a></li>
-			<li><a href="#">RossC0</a></li>
-			<li><a href="#">Wombert</a></li>
+			<li class="op"><a href="#"><img src="images/icons/vcard.png" alt="" /></a>AgaviBot</li>
+			<li class="voice"><a href="#"><img src="images/icons/vcard.png" alt="" /></a>splatch</li>
+			<li class="voice"><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>impl</li>
+			<li><a href="#"><img src="images/icons/vcard.png" alt="" /></a>CIA-20</li>
+			<li><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>eremit</li>
+			<li><a href="#"><img src="images/icons/vcard.png" alt="" /></a>GMFlash</li>
+			<li><a href="#"><img src="images/icons/vcard.png" alt="" /></a>horros</li>
+			<li><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>kaos</li>
+			<li><a href="#"><img src="images/icons/vcard.png" alt="" /></a>luke`_</li>
+			<li><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>MrJeep</li>
+			<li><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>RossC0</li>
+			<li><a href="#"><img src="images/icons/vcard.png" alt="" /></a>ttj</li>
+			<li><a href="#"><img src="images/icons/vcard.png" alt="" /></a>um</li>
+			<li><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>v-dogg</li>
+			<li><a href="#"><img src="images/icons/email_edit.png" alt="" /></a><a href="#"><img src="images/icons/vcard.png" alt="" /></a>Wombert</li>
 		</ul>
+	</li>
+	<li>
+		<div>#propel (12)</div>
+	</li>
+	<li>
+		<div>#prototype (48)</div>
+	</li>
+	<li>
+		<div>##textmate (73)</div>
 	</li>
 </ul>
