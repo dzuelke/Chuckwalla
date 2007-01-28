39f3f44045cf7f200c9b4812229521948d581764
ptdtv8yr <ptdtv8yr@6f7ed374-3227-0410-bbb7-b8272cb5e44e>
2007-01-28 06:02:21 +0000

    404 beautified

--- app/modules/Web/templates/Error404Success.php
+++ app/modules/Web/templates/Error404Success.php
@@ -1,9 +1,21 @@
-<html>
-	<head>
-		<title>404 Not Found</title>
-	</head>
-	<body>
-		<h1>404 Not Found</h1>
-		<p>The page you requested could not be found.</p>
-	</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"  xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<title>Chuckwalla Bot</title>
+	<link rel="stylesheet" type="text/css" href="/css/base.css?ver=2" />
+	<link rel="stylesheet" type="text/css" href="/css/screen.css?ver=2" />
+	<link rel="stylesheet" type="text/css" media="print" href="/css/print.css?ver=2" />
+</head>
+<body>
+	<div id="header">
+		<h1 class="siteHeader"><?php echo AgaviConfig::get('core.app_name'); ?></h1>
+	</div>
+	<div id="content">
+		<div id="core404">
+			<h1>404 Not Found</h1>
+			<p>The page you requested could not be found.</p>
+		</div>
+	</div>
+</body>
 </html>
