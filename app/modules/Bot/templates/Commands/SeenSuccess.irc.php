af7775a80edadb5ee33508a1f05ff3fcfb191665
ptdtv8yr <ptdtv8yr@6f7ed374-3227-0410-bbb7-b8272cb5e44e>
2007-01-28 05:26:16 +0000

    template fix

--- app/modules/Bot/templates/Commands/SeenSuccess.irc.php
+++ app/modules/Bot/templates/Commands/SeenSuccess.irc.php
@@ -1,5 +1,1 @@
-<?php if($t['result']['is_online']): ?> 
-The user <?php echo $t['nickname']; ?> is currently online
-<?php else: ?>
-The user <?php echo $t['nickname']; ?> was last seen at <?php /*$tm->_d($t['last_quit_time']); */?>
-<?php endif; ?>
+<?php if($t['result']['is_online']): ?>The user <?php echo $t['nickname']; ?> is currently online<?php else: ?>The user <?php echo $t['nickname']; ?> was last seen at <?php /*$tm->_d($t['last_quit_time']); */?><?php endif; ?>
